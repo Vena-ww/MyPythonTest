5ce0e9490fbd5d90c80826477471d92b08dde38f
ww_mm <2578864904@qq.com>
2020-08-18 13:57:48 +0800

    编码配置文件

--- Python_test1/calculate/calc_1/conftest.py
+++ Python_test1/calculate/calc_1/conftest.py
@@ -1,9 +1,12 @@
 from typing import List
+import pytest
 
 
+# hook函数
 def pytest_collection_modifyitems(
         session: "Session", config: "Config", items: List["Item"]
 ) -> None:
+    # 修改编码
     for item in items:
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
