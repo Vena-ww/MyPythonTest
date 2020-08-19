35933c776e8a4461089e2ec440764301d51edea9
ww_mm <2578864904@qq.com>
2020-08-19 11:20:43 +0800

    fixture及编码配置文件

--- Python_test1/calculate/calc_2/conftest.py
+++ Python_test1/calculate/calc_2/conftest.py
@@ -1,6 +1,5 @@
 from typing import List
 import pytest
-import yaml
 from MyPythonTest.Python_test1.calculate.calc_2.calculate import Calculator
 
 
@@ -18,22 +17,6 @@ def get_calc():
     calc = Calculator()
     yield calc
     print('结束计算')
-
-# 数据驱动  从yaml文件读取测试数据
-# def get_data():
-#     # 获取测试数据的绝对路径
-#
-#     with open('calc.yml', encoding='utf-8') as f:
-#         mydatas = yaml.safe_load(f)
-#         # print(mydatas)
-#         # 加法的数据
-#         adddatas = mydatas['add']['datas']
-#         myids = mydatas['add']['myids']
-#
-#         # 除法的数据
-#         divdatas = mydatas['divide']['datas']
-#         divids = mydatas['divide']['myids']
-#     return [adddatas, myids], [divdatas, divids]
 
 # 获取get_data()方法的返回值，作为参数传入fixture方法
 # @pytest.fixture(params=get_data()[0], ids=get_data()[1])
